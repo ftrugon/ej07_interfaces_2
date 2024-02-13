27b5a251991c3d3e8df2a1c412f129e7c5763b65
franconil <franconil04@gmail.com>
2024-02-13 01:07:33 +0100

    interfaces mejor en e lugar de la clase

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,31 @@
+interface Notificable {
+    fun enviarnotificacion()
+}
+
+class CorreoElectronico:Notificable{
+    override fun enviarnotificacion() {
+        println("Se ha enviado una notificacion al correo")
+    }
+}
+
+class MensajeTexto:Notificable{
+    override fun enviarnotificacion() {
+        println("Se ha enviado un mensaje de texto")
+    }
+}
+
+class NotificacionPush:Notificable{
+    override fun enviarnotificacion() {
+        println("Se ha pusheado una notificacion")
+    }
+}
+
 fun main() {
-    println("Hello World!")
+
+    val notificaciones  = mutableListOf<Notificable>()
+    notificaciones.add(CorreoElectronico())
+    notificaciones.add(MensajeTexto())
+    notificaciones.add(NotificacionPush())
+
+    notificaciones.forEach { it.enviarnotificacion() }
 }
